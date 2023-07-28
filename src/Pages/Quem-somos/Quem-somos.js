5b1943c624cd504eef819fd0250cea0712b78a38
Felipe Dias <felipe.00.dias@gmail.com>
2023-07-28 17:15:20 -0300

    refinamento v.41

--- src/Pages/Quem-somos/Quem-somos.js
+++ src/Pages/Quem-somos/Quem-somos.js
@@ -24,20 +24,7 @@ function QuemSomos() {
            <div class="hoodie-guy"></div>
            <div class="circle">
            <span style="--i:1;"><img src={prisma} alt='' height="75px"/></span>
-             <span style="--i:1;"><img src="assets/SVGIcons/light-prisma.svg" height="75px"></span>
-              
-               <span style="--i:2;"><img src="assets/SVGIcons/html-5.svg" height="70px"></span>
-               <span style="--i:3;"><img src="assets/SVGIcons/figma.svg" height="75px"></span>
-               <span style="--i:4;"><img src="assets/SVGIcons/css3.svg" height="70px"></span>
-               <span style="--i:5;"><img src="assets/SVGIcons/javascript.svg" height="75px"></span>
-               <span style="--i:5;"><img src="assets/SVGIcons/expressjs-ar21.png" height="70px"></span>
-               <span style="--i:7;"><img src="assets/SVGIcons/react-1.svg" height="75px"></span>
-               <span style="--i:8;"><img src="assets/SVGIcons/MySQL-Dark.svg" height="70px"></span>
-               <span style="--i:9;"><img src="assets/SVGIcons/Firebase-Dark.svg" height="70px"></span>
-               <span style="--i:10;"><img src="assets/SVGIcons/Github-Dark.svg" height="75px"></span>
-               <span style="--i:11;"><img src="assets/SVGIcons/Mongo-db-logo.png" height="60px"></span>
-               <span style="--i:13;"><img src="assets/SVGIcons/nodejs_logo.png" height="75px"></span>
-               <span style="--i:14;"><img src="assets/SVGIcons/jsjpg.jpg" height="75px"></span>
+            
            </div>
        </div>
        <div class="background-circle"></div>
