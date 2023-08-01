5f0fb51ff4f1b80946cb1009c0e20dc4b98fb223
Felipe Dias <felipe.00.dias@gmail.com>
2023-08-01 10:20:13 -0300

    refinamento v.01

--- src/componentes/Menu/Menu.js
+++ src/componentes/Menu/Menu.js
@@ -19,11 +19,21 @@ function Menu() {
                 <li className='efect'><Link to="/galeria">Galeria</Link></li>
                 <li className='efect'><Link to="/planos">Plano Parceria</Link></li>
                 <li><Link to="/cadastros"><button className='cadastro'  >Cadastre-se</button></Link></li>
-               <li><Link to="/login" ><button className='entrar'  >Log-in<i className="fa fa-sign-in" aria-hidden="true"></i></button></Link></li>  
+               <li><Link to="/login" className='entrar'>Login<span class="container">
+               <svg height="24" width="24" viewBox="0 0 24 24">
+               <path d="M0 0h24v24H0z" fill="none"></path>
+               <path d="M16.172 11l-5.364-5.364 1.414-1.414L20 12l-7.778 7.778-1.414-1.414L16.172 13H4v-2z" fill="currentColor"></path>
+               </svg></span></Link></li>  
                 
               </ul>
             </nav>
           </header>
+
+         //<a class="swipe">Login <span class="container">
+        // <svg height="24" width="24" viewBox="0 0 24 24">\\
+        //  <path d="M0 0h24v24H0z" fill="none"></path>
+//<path d="M16.172 11l-5.364-5.364 1.414-1.414L20 12l-7.778 7.778-1.414-1.414L16.172 13H4v-2z" fill="currentColor"></path>
+   //       </svg></span> </a>
           
     
   );
