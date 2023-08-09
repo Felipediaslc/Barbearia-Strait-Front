aab092d016aa76925af4bc44fa7aca50f90a93fc
Felipe Dias <felipe.00.dias@gmail.com>
2023-08-09 14:04:13 -0300

    refinamento v.10

--- src/componentes/Menu/Menu.js
+++ src/componentes/Menu/Menu.js
@@ -19,7 +19,7 @@ function Menu() {
                 <li className='efect'><Link to="/galeria">Galeria</Link></li>
                 <li className='efect'><Link to="/planos">Plano Parceria</Link></li>
                 <li><Link to="/cadastros"><button className='cadastro'>Cadastre-se</button></Link></li>
-               <li ><Link to="/login" ><button className='button'><span>Hover me!</span></button> </Link></li> </ul>
+               <li ><Link to="/login" ><button className='button'><span>Login</span></button> </Link></li> </ul>
             </nav></header>
 
           
